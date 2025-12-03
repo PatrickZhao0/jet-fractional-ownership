4984841c4463e067119bf4491631070b9f69bfdd
Bailong Zhao <bz2099@nyu.edu>
2025-12-03 19:21:42 +0800

    MockJET-O for unit testing

---
+++ contracts/MockTokens/MockJETO.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockJETO is ERC20 {
+    constructor() ERC20("Mock JETO", "mJETO") {}
+    // Fake JETO Contract(For Unit Test) 
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
